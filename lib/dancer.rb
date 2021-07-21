5ba8381630582dc5885f6f9797d505878e35ca20
Brian Smith <brian.bubbam@gmail.com>
2021-07-21 14:19:15 -0500

    commit tree fd2c6eadfea70f5469b78d216d7186a653a39910

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,18 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+
+
+class Dancer
+    #include Dance  #To lend our two classes all of the methods of the Dance module, we use the include keyword:
+    extend MetaDancing #extend this module into both the Kid and Dancer classes
+
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+
+
+end
